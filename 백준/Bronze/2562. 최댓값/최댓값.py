ede0b8b579895c13180ba48cf7e0c0efd21f120a
huge-head-dino <129301830+huge-head-dino@users.noreply.github.com>
2023-04-11 21:16:34 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,13 @@
+a= int(input())
+b= int(input())
+c= int(input())
+d= int(input())
+e= int(input())
+f= int(input())
+g= int(input())
+h= int(input())
+i= int(input())
+
+aa=[a,b,c,d,e,f,g,h,i]
+print(max(aa))
+print(aa.index(max(aa))+1)
