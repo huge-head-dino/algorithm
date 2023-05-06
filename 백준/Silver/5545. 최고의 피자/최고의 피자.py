5e120be48976e73e77878dfa80b22faef0f18e75
huge-head-dino <129301830+huge-head-dino@users.noreply.github.com>
2023-05-06 11:39:12 +0900

    [Silver III] Title: 최고의 피자, Time: 48 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Silver/5545. 최고의 피자/최고의 피자.py
@@ -0,0 +1,26 @@
+import sys
+import math
+
+input = sys.stdin.readline
+
+N = int(input())
+PR_D, PR_T = map(int,input().split())
+dough = int(input())
+toppings = []
+res=0
+
+
+for i in range(N):
+    topping = int(input())
+    toppings.append(topping)
+# 내림차순으로 만들어줘야만 아래 슬라이스가 성립
+# 1개만 고른놈중엔 index[0]이 젤 효율적
+# 2개만 고른놈중엔 index[0]하고 [1]이 젤 효율적
+jung = sorted(toppings, reverse=True)
+
+for i in range(len(jung)+1):
+    price = PR_D + PR_T * (i)
+    res = max(res, (dough+sum(jung[:(i)]))//price)
+
+
+print(res)
