78d71a0e201d84f1d12fad715b2d4260e09519d6
huge-head-dino <129301830+huge-head-dino@users.noreply.github.com>
2023-09-04 15:23:18 +0900

    [level 1] Title: 약수의 합, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    for(let i = 1; i <= (n/2); i++){
+        if(n % i == 0){
+            answer += i
+        }
+    }
+    answer += n
+    return answer;
+}
