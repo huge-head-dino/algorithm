88432708e4d1baa8c97b56d940753bfd565d153e
huge-head-dino <129301830+huge-head-dino@users.noreply.github.com>
2023-11-02 23:54:52 +0900

    [level unrated] Title: rny_string, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.py
@@ -0,0 +1,8 @@
+def solution(rny_string):
+    answer = ''
+    if 'm' in rny_string :
+        answer = rny_string.replace('m','rn')
+    else:
+        answer = rny_string
+        
+    return answer
