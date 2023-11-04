383c1f5ccd26ad29eb077c78e18ba237e90b8d60
huge-head-dino <129301830+huge-head-dino@users.noreply.github.com>
2023-11-04 15:48:13 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,19 @@
+def solution(num):
+    res = 0
+    # num이 1일 경우 0을 return
+    if num == 1:
+        return 0
+    # 500번 반복
+    for _ in range(500):
+        # num이 짝수라면 num을 2로 나눈다.
+        if num % 2 == 0:
+            num = num / 2
+        elif num % 2 == 1:
+            num = (num * 3) + 1
+        res += 1 
+        if num == 1:
+            return res
+        
+        
+    if res >= 500 and num != 1:
+        return -1
