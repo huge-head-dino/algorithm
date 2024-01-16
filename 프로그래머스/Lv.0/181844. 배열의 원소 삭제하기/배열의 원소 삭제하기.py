53bd44ff961d339e0ba9fcf4659da91a5f7b5734
JUNHO SHIN <129301830+huge-head-dino@users.noreply.github.com>
2024-01-16 15:21:55 +0900

    [level Lv.0] Title: 배열의 원소 삭제하기, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -0,0 +1,7 @@
+def solution(arr, delete_list):
+    answer = arr.copy()
+    for i in arr:
+        for j in delete_list:
+            if i == j:
+                answer.remove(i)
+    return answer
