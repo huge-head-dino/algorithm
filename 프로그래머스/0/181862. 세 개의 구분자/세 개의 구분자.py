d383516fc5fca433c43a46b04f03456ae85cfd70
JUNHO SHIN <129301830+huge-head-dino@users.noreply.github.com>
2024-09-01 17:35:53 +0900

    [level 0] Title: 세 개의 구분자, Time: 105.37 ms, Memory: 21.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -0,0 +1,24 @@
+def solution(myStr):
+    answer = []
+    temp = []
+    
+    # ABC로만 이뤄졌으면 EMPTY출력
+    if all(j in 'abc' for j in myStr):
+        return ["EMPTY"]
+    
+    for i in myStr:
+        if i in 'abc':
+            if temp:
+                piece = ''.join(temp)
+                answer.append(piece)
+                temp = []
+        else:
+            temp.append(i)
+    # 마지막 temp에 남아있는 부분을 처리
+    if temp:
+        piece = ''.join(temp)
+        answer.append(piece)
+    
+    return answer
+            
+    
