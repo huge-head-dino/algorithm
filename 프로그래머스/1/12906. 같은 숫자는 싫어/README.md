ecbc037321f6e8236b9d1da039ad95fd692481c5
JUNHO SHIN <129301830+huge-head-dino@users.noreply.github.com>
2025-01-09 15:55:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 108.38 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 27.8 MB, 시간: 172.65 ms
+메모리: 36 MB, 시간: 108.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 12일 16:45:08
+2025년 01월 09일 15:55:36
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,18 +1,16 @@
+from collections import deque
 def solution(arr):
-    answer = 0
-    stack = []
-    for i in arr:
-        # 비어 있을 때, pop erorr 방지
-        if len(stack)== 0:
-            stack.append(i)
-            continue
-            
-        c = stack.pop()
-        if i == c:
-            stack.append(c)
-            continue
-        elif i != c:
-            stack.append(c)
-            stack.append(i)
+    left = []
+    temp = arr.copy()
+    right = deque(temp)
     
-    return stack
+    for _ in arr:
+        test = right.popleft()
+        if not left:
+            left.append(test)
+            continue
+        if left[-1] != test:
+            left.append(test)
+        else:
+            pass
+    return left
